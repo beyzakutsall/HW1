61d217635a77b59915b7bcaf0e15507dc13707c4
beyzakutsall <beyzaaysenurkutsal@gmail.com>
2025-03-23 01:52:52 +0300

    Update Hw1-Q4a.c

--- Hw1-Q4a.c
+++ Hw1-Q4a.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void computeHomeValue(void) {
+int computeHomeValue(int popularity, int size) {
+    return (popularity * popularity * popularity + size * size) * 10000;
+}
+
+int main() {
     int popularity, size;
     printf("Enter popularity: ");
     scanf("%d", &popularity);
     printf("Enter size: ");
     scanf("%d", &size);
-    printf("Home value is: %d\n", (popularity * popularity * popularity + size * size) * 10000);
-}
-
-int main() {
-    computeHomeValue();
-    system("pause");
+    printf("Home value is: %d\n", computeHomeValue(popularity, size));
     return 0;
 }
